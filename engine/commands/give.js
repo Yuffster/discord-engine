47b80d3258ca5ea979d18f072026630de5588b2b
Yuffster <msteigerwalt@gmail.com>
2010-09-19 06:02:43 +0000

    Rudimentary 'give' command -- need to add a target to the misnamed 'genderize' method.

--- engine/commands/give.js
+++ engine/commands/give.js
@@ -2,8 +2,18 @@ new Class({
 
 	Extends: Command,
 
-	execute: function(content) {
-		this.emit('%You say%s: '+content);
+	execute: function(args) {
+		if (!args.match(' to ')) return "Give what to whom?";
+		var parts  = args.split(' to ');
+		var thing  = parts[0].trim();
+		var target = parts[1].trim();
+		var item   = this.getItem(thing);
+		if (!item) return "You don't have that.";
+		var target = this.room.getLiving(target);
+		if (!target) return "Can't find "+target+".";
+		this.removeItem(item);
+		target.addItem(item);
+		this.emit("%You hand%s "+item.get('short')+" to "+target.get('short')+".");
 		return true;
 	}
 
