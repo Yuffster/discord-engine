b392a94f2952713036dd1158e41a9c801ad23edd
Michelle Steigerwalt <msteigerwalt@gmail.com>
2011-05-14 20:37:26 -0400

    Pushed describe out as a published NPM package.

--- tests/run_tests.js
+++ tests/run_tests.js
@@ -1,41 +1,10 @@
 require(__dirname+'/../engine');
-
-describe = function(series, obj) {
-
-	sys.puts(series.toUpperCase().color('yellow')+":");
-
-	var passed = 0, failed = 0, total = 0;
-
-	Object.each(obj, function(test, desc) {
-
-		total++;
-		try {
-			test();
-		} catch (e) {
-			failed++;
-			sys.puts("\t"+desc.color('red'));
-			sys.puts("\t\t====> "+e.stack);
-			return;
-		}
-
-		passed++;
-
-		sys.puts("\t"+desc.color('green'));
-
-	});
-
-	sys.puts(
-		"PASSED".color('green')+": "+passed+
-		" / "+total
-	);
-
-
-};
+require("describe");
 
 assert = require('assert');
 
 (['messaging', 'advanced_parser']).each(function(test_module) {
-	require(__dirname+"/"+test_module);
+	require(__dirname+"/tests/"+test_module);
 });
 
 process.exit();
